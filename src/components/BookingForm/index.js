f4086d25a9d053baa5998ffce0895ebb5fc8012f
Rajaram Manikanta <rajarammanikantamani@gmail.com>
2023-08-23 21:00:17 +0530

    First Commit for Movie Show Application

--- src/components/BookingForm/index.js
+++ src/components/BookingForm/index.js
@@ -8,22 +8,48 @@ import Footer from '../Footer'
 class BookingForm extends Component{
 
 
-    renderNavbar=()=>(
-        <nav className='navbar'>
-            <p>Lights, Camera, Enjoy!</p>
-        </nav>
-    )
-    renderFooter=()=>(
-        <div className='footer'>
-            <p>Movies can make you feel things you've never felt, and sometimes, they even make you attracted to a different world</p>
-        </div>
-    )
 
     render(){
         return(
             <div className='booking-container'> 
       <Navbar/>
-      <p>Fill Your Details</p>
+      <p className='heading'>Fill Your Details</p>
+      <form className='form-container'>
+        <div>
+        <label htmlFor='name'>Name:</label>
+        <br/>
+        <input type="text" id="name"/>
+        </div> 
+        <div>
+            <label htmlFor='email'>Email:</label>
+            <br/>
+            <input type="email"/>
+        </div>
+         <div>
+            <label htmlFor='no_of_person'>No Of PERSONS</label>
+            <br/>
+            <input type="text"/> 
+         </div>
+         <div>
+            <label htmlFor='movie'>Choose Movie</label>
+            <br/>
+            <select id="movie">
+                <option>All Stars</option>
+                <option>All Stars</option>
+                <option>All Stars</option>
+                <option>All Stars</option>
+            </select>
+         </div>
+         <div>
+            <label htmlFor='phone'>Phone:</label>
+            <br/> 
+            <input type="text" id="phone"/>
+         </div>
+         <div>
+            <button type="submit" className='book-button'>Book Now</button>
+         </div>
+         
+      </form>
              <Footer/>
             </div>
 
